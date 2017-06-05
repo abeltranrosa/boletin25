90110863fab7c3341bbf8e2370f7efcb5b3d1775
antonio <antonio@192.168.0.11>
2017-06-05 12:28:34 +0200

    boletin25 apartado 3 main

---
+++ boletin25.1/Boletin253/src/boletin253/Boletin253.java
@@ -0,0 +1,15 @@
+
+
+package boletin253;
+
+
+public class Boletin253 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FBoletin253 obx=new FBoletin253();
+        obx.setVisible(true);
+    }
+    
+}
